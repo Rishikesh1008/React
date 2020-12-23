50e47f1e942d23e053df036cde7e5f74f35ebc5b
Rishikesh1008 <rish11bhagate@gmail.com>
2020-12-23 20:12:59 +0530

    I am passionate

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          I am learning react ! 
+          I am learning react ! And I am passionate !
         </a>
       </header>
     </div>
